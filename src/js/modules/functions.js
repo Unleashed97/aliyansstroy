8e4551353ea9f6f3919ad51535d199043a10a843
Roman <noudev@yandex.ru>
2023-10-22 13:45:55 +0300

    feat: add modal

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -19,20 +19,65 @@ export const handleBurger = () => {
 }
 
 export const handleModal = () => {
-    const modal = document.querySelector('.modal')
+    const modals = document.querySelectorAll('.modal')
+    const modalOpenBtns = document.querySelectorAll('.modal-open-btn')
 
-    if (modal) {
-        const modalCloseBtn = modal.querySelector('.modal__close')
+    if (modalOpenBtns.length) {
+        modalOpenBtns.forEach((btn) =>
+            btn.addEventListener('click', () => {
+                const modal = document.querySelector(
+                    `.${btn.getAttribute('data-target')}`,
+                )
 
-        if (modalCloseBtn) {
-            modalCloseBtn.addEventListener('click', () => {
-                modal.classList.remove('active')
-            })
-        }
-        window.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                modal.classList.remove('active')
-            }
+                console.log(modal)
+
+                if (modal) {
+                    modal.classList.add('active')
+                    // document.body.classList.add('fixed')
+
+                    const scrollY =
+                        document.documentElement.style.getPropertyValue(
+                            '--scroll-y',
+                        )
+                    modal.style.top = `${scrollY}`
+                    const body = document.body
+                    body.style.position = 'fixed'
+                    body.style.top = `-${scrollY}`
+
+                    const modalCloseBtn =
+                        modal.querySelector('.modal__btn-close')
+
+                    if (modalCloseBtn) {
+                        modalCloseBtn.addEventListener('click', () => {
+                            modal.classList.remove('active')
+                            // document.body.classList.remove('fixed')
+
+                            const body = document.body
+                            const scrollY = body.style.top
+                            body.style.position = ''
+                            window.scrollTo(0, parseInt(scrollY || '0') * -1)
+                        })
+                    }
+                    window.addEventListener('click', (e) => {
+                        if (e.target === modal) {
+                            modal.classList.remove('active')
+                            // document.body.classList.remove('fixed')
+
+                            const body = document.body
+                            const scrollY = body.style.top
+                            body.style.position = ''
+                            window.scrollTo(0, parseInt(scrollY || '0') * -1)
+                        }
+                    })
+                }
+            }),
+        )
+
+        window.addEventListener('scroll', () => {
+            document.documentElement.style.setProperty(
+                '--scroll-y',
+                `${window.scrollY}px`,
+            )
         })
     }
 }
